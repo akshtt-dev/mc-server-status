847909a1699aa43cf7ae233d62a3f8ba557c4ced
akshtt-dev <maillegendop@gmail.com>
2024-06-20 11:04:40 +0530

    chore: Update serverIP display in script.js

--- public/js/home/script.js
+++ public/js/home/script.js
@@ -1,75 +1,91 @@
-const mcstatus = document.getElementById('status');
-const players = document.getElementById('players');
-const version = document.getElementById('version');
-const serverIP = document.getElementById('serverIp');
-const playersList = document.getElementById('playersList');
-const statusContainer = document.getElementById('statusContainer');
-const serverName = document.getElementById('serverName');
+const mcstatus = document.getElementById("status");
+const players = document.getElementById("players");
+const version = document.getElementById("version");
+const serverIP = document.getElementById("serverIp");
+const playersList = document.getElementById("playersList");
+const statusContainer = document.getElementById("statusContainer");
+const serverName = document.getElementById("serverName");
 
 class serverApi {
-    constructor(serverIP) {
-        this.serverIP = serverIP;
-    }
-    getServerInfo() {
-        fetch(`https://api.mcsrvstat.us/3/${this.serverIP}`)
-            .then((res) => res.json())
+  constructor(serverIP) {
+    this.serverIP = serverIP;
+  }
+  getServerInfo() {
+    fetch(`https://api.mcsrvstat.us/3/${this.serverIP}`)
+      .then((res) => res.json())
+      .then((data) => {
+        serverIP.innerText = this.serverIP;
+        if (data.online) {
+          mcstatus.textContent = "online";
+          fetch("/post", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ serverIp: this.serverIP }),
+          })
+            .then((response) => response.text())
             .then((data) => {
-                serverIP.innerText = this.serverIP;
-                if (data.online) {
-                    mcstatus.textContent = 'online';
-                } else {
-                    mcstatus.textContent = 'offline';
-                }
-                if (data.players) {
-                    players.innerHTML = `${data.players.online}/${data.players.max}`;
-                } else {
-                    players.innerHTML = '0/0';
-                }
-                if (data.version)  {
-                    version.innerHTML = data.version;
-                } else {
-                    version.innerHTML = 'N/A';
-                }
-                if (data.icon) {
-                    statusContainer.style.backgroundImage = `url(${data.icon})`;
-                } else {
-                    statusContainer.style.backgroundImage = 'url(./src/serverError.avif)';
-                }
+              console.log(data);
             })
-            .catch((err) => {
-                console.error(err);
+            .catch((error) => {
+              console.error("Error:", error);
             });
-    }
+        } else {
+          mcstatus.textContent = "offline";
+        }
+        if (data.players) {
+          players.innerHTML = `${data.players.online}/${data.players.max}`;
+        } else {
+          players.innerHTML = "0/0";
+        }
+        if (data.version) {
+          version.innerHTML = data.version;
+        } else {
+          version.innerHTML = "N/A";
+        }
+        if (data.icon) {
+          statusContainer.style.backgroundImage = `url(${data.icon})`;
+        } else {
+          statusContainer.style.backgroundImage = "url(./src/serverError.avif)";
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 }
 
 const server1 = new serverApi(serverIP.innerText);
 server1.getServerInfo();
 
-const serverInfo = document.getElementsByClassName('serverInfo');
+const serverInfo = document.getElementsByClassName("serverInfo");
 for (let i = 0; i < serverInfo.length; i++) {
-    serverInfo[i].addEventListener('click', function() {
-            var copyText = serverInfo[i].innerText;
-            navigator.clipboard.writeText(copyText);
-            copyDiv.hidden = false;
-            setTimeout(() => {
-                copyDiv.hidden = true;
-            }, 2000);
-    });
+  serverInfo[i].addEventListener("click", function () {
+    var copyText = serverInfo[i].innerText;
+    navigator.clipboard.writeText(copyText);
+    copyDiv.hidden = false;
+    setTimeout(() => {
+      copyDiv.hidden = true;
+    }, 2000);
+  });
 }
 
-const copyDiv = document.getElementById('copyDiv');
+const copyDiv = document.getElementById("copyDiv");
 copyDiv.hidden = true;
 
-const submitBtn = document.getElementById('submitBtn');
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const serverIpBox = document.getElementById('serverIpBox');
-    if (serverIpBox.value.trim() === '') {
-        const updateServer = new serverApi("mbs.berrysmp.net");
-        updateServer.getServerInfo();
-    } else {
-        const updateServer = new serverApi(serverIpBox.value);
-        updateServer.getServerInfo();
-    }
-    serverIpBox.value = '';
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const serverIpBox = document.getElementById("serverIpBox");
+  const serverIp = serverIpBox.value.trim();
+
+  if (serverIp === "") {
+    const updateServer = new serverApi("mbs.berrysmp.net");
+    updateServer.getServerInfo();
+  } else {
+    const updateServer = new serverApi(serverIp);
+    updateServer.getServerInfo();
+  }
+  serverIpBox.value = "";
 });
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,34 @@
 import express from 'express';
 import serverList from '../models/serverList.js';
 const router = express.Router();
+const app = express();
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 // Home page route
 router.get('/', (req, res) => {
     res.render('home', { title: 'MC Server Status' });
-    const newServer = new serverList({serverIp: 'Create a new Minecraft server', id: Math.floor(Math.random() * 1000)});
-    newServer.save().then(() => console.log('ServerIP added!')).catch(err => console.log(err));
+});
+
+router.post('/post', async (req, res) => {
+    const randomId = Math.floor(Math.random() * 1000);
+    const server = new serverList({ serverIp: req.body.serverIp, id: randomId }); 
+    await server.save().then(result => {
+        res.send('Your server has been added to the database!');
+        console.log(`Server added to the database ${req.body.serverIp}`)
+})
+    .catch(err => {
+        res.send('There was an error adding your server to the database');
+        console.log(err)
+    });
+});
+
+// Server List page route
+router.get('/serverlist', (req, res) => {
+    serverList.find().then(result => {
+        res.render('serverlist', { title: 'Server List', servers: result });
+    }).catch(err => console.log(err));
 });
 
 // About Us page route
